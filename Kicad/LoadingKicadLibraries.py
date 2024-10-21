6b819a79f12b5038123934ca6322b2391f11c414
jacob-bk1 <jacobbrotmankrass@bluerobotics.com>
2024-10-21 14:22:08 -0700

    Modified BRE generator to be consecutive, added functionality to update Vendor table in database from Kicad libs

--- Kicad/LoadingKicadLibraries.py
+++ Kicad/LoadingKicadLibraries.py
@@ -1,4 +1,5 @@
 import kiutils.symbol
+import re
 
 lib_path = "C:/Users/JacobBrotmanKrass/Documents/GitHub/br-kicad-lib/Symbols/BR_Capacitors_0201.kicad_sym"
 lib_obj = kiutils.symbol.SymbolLib()
@@ -8,7 +9,16 @@ symbols = sym_lib.symbols
 
 for symbol in symbols:
     properties = {property.key: property.value for property in symbol.properties}
-    print(properties)
+    print(symbol.libId)
+    supplier_properties = {property: properties[property] for property in properties if property[:8]=="Supplier"}
+    supplier_number = {supp_prop: supplier_properties[supp_prop] for supp_prop in supplier_properties if supp_prop[9]=='P'}
+    supplier_names = {supp_prop: supplier_properties[supp_prop] for supp_prop in supplier_properties if supp_prop[9]!='P'}
+    print(supplier_names, supplier_number)
+
+    for name in supplier_names:
+        for number in supplier_number:
+            if name[-1] == number[-1]:
+                print(name, number)
 
 
 
--- Kicad/MigrateKicadToDatabase.py
+++ Kicad/MigrateKicadToDatabase.py
@@ -6,16 +6,22 @@ import glob
 
 #### Function for generating a unique BR ID ####
 def generate_BRID(cursor):
-    num_digits = 4
-    BR_num = []
+
+    # Set number of digits in unique BRID
+    num_digits = 4      
+    
+    # Gather all existing BRID's from the databasde
     cursor.execute('SELECT "ID" FROM public."Parts";')
     existing_BRIDs = [row[0] for row in cursor.fetchall()]
-    while 1:
-        BR_num = []
-        for i in range(num_digits):
-            BR_num.append(random.randint(0,9))
-        BRID = 'BREE' + ''.join(map(str, BR_num))
 
+    # Increment by 1 until the BRID doesn't exist
+    for n in range(10**num_digits):
+
+        # Convert to a zero padded string (to fill the desired number of digits -- e.g, 1 --> '0001')
+        BR_num = str(n).zfill(num_digits)
+        BRID = 'BREE' + BR_num
+
+        # If the new BRID isn't currently used, we've found the one!
         if BRID not in existing_BRIDs:
             break
 
@@ -45,9 +51,9 @@ for part in parts_table:
 SYMBOLS_PATH = "C:/Users/JacobBrotmanKrass/Documents/GitHub/br-kicad-lib/Symbols"
 os.chdir(SYMBOLS_PATH)
 for lib_file in glob.glob("*.kicad_sym"):
-    print(lib_file)
 
     lib_nickname = lib_file.replace(".kicad_sym", "")
+    print(lib_nickname)
     lib_path = os.path.join(SYMBOLS_PATH, lib_file)
     lib_obj = kiutils.symbol.SymbolLib()
     sym_lib = lib_obj.from_file(lib_path)
@@ -70,9 +76,31 @@ for lib_file in glob.glob("*.kicad_sym"):
         bad_char = b'\xc3\x82'.decode()
         print(properties)
         properties["Description"] = properties["Description"].replace(bad_char, '')
+        if "Manufacturer" in properties:
+            manufacturer = properties["Manufacturer"]
+            mpn = properties["Manufacturer Part Num"]
+        else:
+            manufacturer = "None"
+            mpn = "None"
 
         cursor.execute('SELECT * FROM public."Parts";')
-        cursor.execute('INSERT INTO public."Parts"("ID", "Description", "Symbols", "Footprints", "Value", "Datasheet", "Category") VALUES(%s, %s, %s, %s, %s, %s, %s)', (BR_ID, properties["Description"], symbol_path, properties["Footprint"], properties["Value"], properties["Datasheet"], category))
+        cursor.execute('INSERT INTO public."Parts"("ID", "Name", "Description", "Value", "Symbol", "Footprint",  "Datasheet", "Manufacturer", "MPN", "Category") VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (BR_ID, symbol.libId, properties["Description"], properties["Value"], symbol_path, properties["Footprint"], properties["Datasheet"], manufacturer, mpn, category))
+        
+        print(properties["Description"])
+
+        supplier_properties = {property: properties[property] for property in properties if property[:8]=="Supplier"}
+        supplier_numbers = {supp_prop: supplier_properties[supp_prop] for supp_prop in supplier_properties if supp_prop[9]=='P'}
+        supplier_names = {supp_prop: supplier_properties[supp_prop] for supp_prop in supplier_properties if supp_prop[9]!='P'}
+        print(supplier_names, supplier_numbers)
+
+        null_strings = ["", " ", "-", "--", "~", "NA", "N/A"]
+        for name in supplier_names:
+            for number in supplier_numbers:
+                if name[-1] == number[-1]:
+                    if supplier_numbers[number] not in null_strings:
+                        cursor.execute('SELECT * FROM public."Vendors";')
+                        cursor.execute('INSERT INTO public."Vendors"("ID", "Supplier", "SPN", "Stock") VALUES(%s, %s, %s, %s)', (BR_ID, supplier_names[name], supplier_numbers[number], 0))
+                
 
 
 
